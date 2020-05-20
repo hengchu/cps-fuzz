278690969d5d00064460a41dddb5914b53ef0b89
hengchu <hengchu.zhang@gmail.com>
2020-05-20 14:48:17 -0400

    add some log statements

--- app/Main.hs
+++ app/Main.hs
@@ -5,28 +5,33 @@ import Examples
 import Extraction
 import ExtractionPretty
 
+logWriteFile :: String -> String -> IO ()
+logWriteFile filePath prog = do
+  putStrLn $ "writing to: " ++ filePath
+  writeFile filePath prog
+
 main :: IO ()
 main = do
   let Right py = compileAndExtract "db" bag_filter_sum_noise
-  writeFile "extracted/bag_filter_sum_noise.py" (pExtractionStr py)
+  logWriteFile "extracted/bag_filter_sum_noise.py" (pExtractionStr py)
 
   let Right py = compileAndExtract "db" (kmeans_iter initialCentroids)
-  writeFile "extracted/kmeans_iter.py" (pExtractionStr py)
+  logWriteFile "extracted/kmeans_iter.py" (pExtractionStr py)
 
   let Right py = compileAndExtract "db" (logistic_iter [0.0, 0.0, 0.0, 0.0, 0.0] 0.001)
-  writeFile "extracted/logistic_iter.py" (pExtractionStr py)
+  logWriteFile "extracted/logistic_iter.py" (pExtractionStr py)
 
   let Right py = compileAndExtract "db" (naive_bayes 10)
-  writeFile "extracted/naive_bayes.py" (pExtractionStr py)
+  logWriteFile "extracted/naive_bayes.py" (pExtractionStr py)
 
   let Right py = compileAndExtract "db" (pca 10 3)
-  writeFile "extracted/pca.py" (pExtractionStr py)
+  logWriteFile "extracted/pca.py" (pExtractionStr py)
 
   let Right py = compileAndExtract "db" (perceptron_iter [0.0, 0.0, 0.0, 0.0, 0.0])
-  writeFile "extracted/perceptron_iter.py" (pExtractionStr py)
+  logWriteFile "extracted/perceptron_iter.py" (pExtractionStr py)
 
   let Right py = compileAndExtract "db" simple_expm
-  writeFile "extracted/simple_expm.py" (pExtractionStr py)
+  logWriteFile "extracted/simple_expm.py" (pExtractionStr py)
 
   let
     fPoints :: forall f. [CPSFuzz f (Number, Number)]
@@ -39,4 +44,4 @@ main = do
     vPoints = [xppair x y | x <- vPoints1, y <- vPoints1]
 
     Right py = compileAndExtract "db" (kmedian_iter fPoints vPoints)
-  writeFile "extracted/kmedian_iter.py" (pExtractionStr py)
+  logWriteFile "extracted/kmedian_iter.py" (pExtractionStr py)
